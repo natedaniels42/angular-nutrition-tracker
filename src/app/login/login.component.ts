d8e444492a4ad411350b27eb97fafb5879615904
Nathan Daniels <nathandaniels@Nathans-MBP.attlocal.net>
2021-12-21 16:56:17 -0600

    Added jwt_decode to parse id of logged in user

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import jwt_decode from 'jwt-decode';
+
 import { User } from '../user';
 import { UserService } from '../user.service';
+
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
@@ -20,7 +23,8 @@ export class LoginComponent implements OnInit {
     this.userService.login(this.user)
       .then((response: any) => {
         console.log(response.token)
-        localStorage.setItem('token', response.token)  
+        localStorage.setItem('id', (jwt_decode(response.token) as any).id);
+        localStorage.setItem('token', response.token);  
       })
   }
 }
