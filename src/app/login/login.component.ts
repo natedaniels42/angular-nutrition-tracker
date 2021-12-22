d16e6514b6562b3fa339128baf3314a2d0641627
Nathan Daniels <nathandaniels@Nathans-MBP.attlocal.net>
2021-12-22 08:52:02 -0600

    Added getUser to user service

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -27,6 +27,11 @@ export class LoginComponent implements OnInit {
         localStorage.setItem('id', (jwt_decode(response.token) as any).id);
         localStorage.setItem('token', response.token);
         this.router.navigate(['/profile']);
+
+        this.userService.getUser(localStorage.getItem('id') as string)
+          .then((response: any) => {
+            localStorage.setItem('user', JSON.stringify(response));
+          })
       })
   }
 }
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -9,13 +9,25 @@ export class UserService {
   private url: string = 'http://localhost:4000/api/v1'
   constructor(private http: HttpClient) { }
 
+  public getUser(id: string) {
+    return new Promise((resolve, reject) => {
+      this.http.get(`${this.url}/user/${id}`).subscribe(
+        (response) => {
+          resolve(response);
+        }, (err) => {
+          reject(err);
+        }
+      )
+    })
+  }
+  
   public createUser(data: User) {
     return new Promise((resolve, reject) => {
       this.http.post(`${this.url}/auth/register`, data).subscribe(
         (response) => {
           resolve(response);
         }, (err) => {
-          reject(err)
+          reject(err);
         })
     }) 
   }
