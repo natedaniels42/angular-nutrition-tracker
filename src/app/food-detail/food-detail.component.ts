6efd3edf5f04206f8dfacfbee9144afe68584a8e
Nathan Daniels <nathandaniels@Nathans-MBP.attlocal.net>
2021-12-28 15:03:37 -0600

    Styling on food-detail

--- src/app/food-detail/food-detail.component.ts
+++ src/app/food-detail/food-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router'
 import { Food } from '../food';
+import { User } from '../user'; 
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'food-detail',
@@ -18,12 +20,36 @@ export class FoodDetailComponent implements OnInit {
     protein: 0,
     image: '',
   }
-  
-  constructor(private route: ActivatedRoute) { }
 
-  ngOnInit(): void {
+  user: User = {
+    name: '',
+    email: '',
+    password: '',
+    dailyCalorieGoal: 0,
+    dailyCarbGoal: 0,
+    dailyFatGoal: 0,
+    dailyProteinGoal: 0,
+    food: []
+  }
+
+  percentages: {calories: number, carbs: number, fat: number, protein: number} = {
+    calories: 0,
+    carbs: 0,
+    fat: 0,
+    protein: 0
+  };
+  
+  constructor(private userService: UserService, private route: ActivatedRoute) { }
+
+  async ngOnInit(): Promise<void> {
     this.food = JSON.parse(localStorage['food'])
-    console.log(this.food);
+    this.user = await this.userService.getUser(localStorage.getItem('id') as string) as User;
+    this.percentages = {
+      calories: Math.round(this.food.calories / this.user.dailyCalorieGoal * 100),
+      carbs: Math.round(this.food.carbs / this.user.dailyCarbGoal * 100),
+      fat: Math.round(this.food.fat / this.user.dailyFatGoal * 100),
+      protein: Math.round(this.food.protein / this.user.dailyProteinGoal * 100)
+    }
   }
 
 }
